645c4e1dcc8bbd5df901beea3b408df8d1e7fd8c
sammygis <adedoyinsamuel25@gmail.com>
2025-03-07 02:02:23 +0100

    db model

--- ecomerce_api/database/model.py
+++ ecomerce_api/database/model.py
@@ -1,31 +1,50 @@
+"""
 
 
-Customers Table
-Column	Type	Description
-id	SERIAL PRIMARY KEY	Unique customer identifier
-customer_id	VARCHAR(20) UNIQUE	Matches Customer_id from CSV
-gender	VARCHAR(10)	Male/Female
-device_type	VARCHAR(20)	Web, Mobile
-login_type	VARCHAR(20)	Member, Guest
-Orders Table
-Column	Type	Description
-id	SERIAL PRIMARY KEY	Unique order ID
-customer_id	VARCHAR(20)	Foreign Key → Customers(customer_id)
-order_date	DATE	Date order was placed
-order_priority	VARCHAR(10)	Critical, High, etc.
-payment_method	VARCHAR(20)	Credit Card, PayPal, etc.
-Products Table
-Column	Type	Description
-id	SERIAL PRIMARY KEY	Unique product identifier
-product_category	VARCHAR(50)	Matches Product_Category from CSV
-product_name	VARCHAR(100)	Matches Product from CSV
-Order Items Table
-Column	Type	Description
-id	SERIAL PRIMARY KEY	Unique order item identifier
-order_id	INTEGER	Foreign Key → Orders(id)
-product_id	INTEGER	Foreign Key → Products(id)
-quantity	INTEGER	Units purchased
-discount	DECIMAL(5,2)	Discount applied (%)
-sales	DECIMAL(10,2)	Sales revenue from the item
-profit	DECIMAL(10,2)	Profit from the item
-shipping_cost	DECIMAL(10,2)	Cost of shipping
+
+
+"""
+
+from sqlalchemy import Column, String, BigInteger, Date, Float, Integer, VARCHAR, DECIMAL
+from db_setup import Base
+from utils import db_info
+
+class Customers(Base):
+    __tablename__ = db_info.Customers.value
+
+    id = Column(autoincrement=True, primary_key=True)
+    customer_id = Column(VARCHAR(20), unique=True)
+    gender = VARCHAR(10)
+    device_type= Column(VARCHAR(20))	
+    login_type = Column(VARCHAR(20))
+
+
+class Order(Base):
+    __tablename__ = db_info.Customers.value
+
+    id = Column(autoincrement=True, primary_key=True)
+    customer_id = Column(VARCHAR(20), unique=True)
+    order_date = Column(Date)
+    order_priority = Column(VARCHAR(10))
+    payment_method = Column(VARCHAR(20)) 
+   
+
+class Product(Base):
+    __tablename__ = db_info.Customers.value
+
+    id = Column(autoincrement=True, primary_key=True)
+    product_category = Column(VARCHAR(50)) 
+    product_name = Column(VARCHAR(100)) 
+
+class OrderITem(Base):
+    __tablename__ = db_info.Customers.value
+
+    id = Column(autoincrement=True, primary_key=True)
+    order_id = Column(Integer, fk= Order(id))	
+    product_id = Column(Integer, fk=PRoduct(id))
+    quantity = Column(DECIMAL(5,2))
+    sales = Column(DECIMAL(10,2))
+    profit = Column(DECIMAL(10,2))
+    shipping_cost = Column(DECIMAL(10,2))
+
+
