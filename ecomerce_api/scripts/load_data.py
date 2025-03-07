19b7a4966619a27a481b7a6db9c0e44f5060066a
sammygis <adedoyinsamuel25@gmail.com>
2025-03-07 18:43:56 +0100

    feat:extract model data df

--- ecomerce_api/scripts/load_data.py
+++ ecomerce_api/scripts/load_data.py
@@ -1,13 +1,12 @@
 """
-
+Author: Ajeyomi Adeodyin Samuel
+Email: adedoyin samuel25@gmail.com
+Date: 07/03/2025
 
 """
-
 import pandas as pd
 
-
-
-def read_data(data_path) -> pd.DataFrame:
+def read_data(data_path:str) -> pd.DataFrame:
     """ Read csv data to dataframe"""
     try:
         df = pd.read_csv(data_path)
@@ -15,20 +14,47 @@ def read_data(data_path) -> pd.DataFrame:
     except ValueError as e:
         print(f"Error: {e}")
 
+def transform_data(data:pd.DataFrame)->pd.DataFrame:
+    """Perfom Basic Data transformation"""
+    # convert all columns to lower case
+    df = data.columns = [col.lower()  for col in data.columns]
+    # drop duplicates data
+    df = df.drop_duplicates()
+    #  conver the order data from object dtype to datetime
+    df = df['order_date'] = pd.to_datetime(df['order_date'])
+    return df
 
 # data modelling 
-def create_customer_table():
-    pass
+def create_customer_dataframe(df):
+    """Extract data for the customer table"""
+    columns = ["customer_id", "gender", "device_type", "customer_login_type"]
+    customer_df = df[columns]
+    return customer_df
 
-def create_product_table():
-    pass
+def create_order_dataframe(df):
+    """Extract data for the order table"""
+    columns = ["customer_id", "order_date", "order_priority", "payment_method"]
+    order_df = df[columns]
+    return order_df
 
-def create_order_table():
-    pass
+def create_product_dataframe(df):
+    """Extract data for the product table"""
+    columns = ["product_category","product"]
+    product_df = df[columns]
+    return product_df
 
-def create_orderitems_Table():
-    pass
+def create_orderitems_dataframe(df):
+    """Extract data for the order items table"""
+    columns = ["quantity","discount","sales","profit","shipping_cost"]
+    orderitems_df = df[columns]
+    return orderitems_df
 
 
 def load_data_db(data:pd.DataFrame):
     pass
+
+
+
+
+
+
